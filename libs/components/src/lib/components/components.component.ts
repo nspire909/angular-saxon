40d1e57105e38392257ea35c90352fb8fe379b68
Nick Marks <nspire909@gmail.com>
2024-04-10 17:12:56 -0500

    package stuff

--- libs/components/src/lib/components/components.component.ts
+++ libs/components/src/lib/components/components.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'lib-components',
+  selector: 'ngsx-components',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './components.component.html',
